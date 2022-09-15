f5d8a5afa030e13467cd2fee014765fdd83bfec3
ImRadiGamer <radicolagamer@gmail.com>
2022-09-15 11:40:42 -0600

    Update TotiosEvo.java
    Le agregue el menos para que la progra jale y no este invertido el control

--- TeamCode/src/main/java/org/firstinspires/ftc/deimos15909/TotiosEvo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/deimos15909/TotiosEvo.java
@@ -29,7 +29,7 @@ public class TotiosEvo extends OpMode {
 
         // Mecanum drive is controlled with three axes: drive (front-and-back),
         // strafe (left-and-right), and twist (rotating the whole chassis).
-        double drive  = gamepad1.left_stick_y;
+        double drive  = -gamepad1.left_stick_y;
         double strafe = gamepad1.left_stick_x;
         double twist  = gamepad1.right_stick_x;
 
