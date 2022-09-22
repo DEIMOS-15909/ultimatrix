095ecf3afd8fdbe897221f9d5fb054650f1adf9d
pepefrog <santiihulk@gmail.com>
2022-09-22 14:42:47 -0600

    -Diego y Santiago pero ahora de otra compu

--- TeamCode/src/main/java/org/firstinspires/ftc/deimos15909/TotiosEvo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/deimos15909/TotiosEvo.java
@@ -9,73 +9,57 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "Ultimatrix", group = "Control")
 public class TotiosEvo extends OpMode {
 
-    DcMotor Izqe;
-    DcMotor Dere;
-    DcMotor Izqa;
-    DcMotor Dera;
-    DcMotor Brazito;
-    DcMotor Elev;
-    DcMotor Elev2;
-    DcMotor Elev3;
-    Servo Garrita;
-    Servo Carpus;
-    Servo Burgir;
-    Servo DeChill2;
-    Servo DeChill;
+    DcMotor izqE;
+    DcMotor derE;
+    DcMotor izqA;
+    DcMotor derA;
+    DcMotor brazo;
+    DcMotor elev;
+    DcMotor elev2;
+    Servo garra;
+    Servo rotador;
 
 
     @Override
     public void init() {
-        Izqe = hardwareMap.dcMotor.get("Izqe");
-        Izqa = hardwareMap.dcMotor.get("Izqa");
-        Dere = hardwareMap.dcMotor.get("Dere");
-        Dera = hardwareMap.dcMotor.get("Dera");
+        izqE = hardwareMap.dcMotor.get("izqE");
+        izqA = hardwareMap.dcMotor.get("izqA");
+        derE = hardwareMap.dcMotor.get("derE");
+        derA = hardwareMap.dcMotor.get("derA");
         //MOTORES CONTROLHUB
 
-        Brazito = hardwareMap.dcMotor.get("Brazito");
-        Elev =  hardwareMap.dcMotor.get("Elev");
-        Elev2 = hardwareMap.dcMotor.get("Elev2");
-        Elev3 = hardwareMap.dcMotor.get("Elev3");
+        brazo = hardwareMap.dcMotor.get("brazo");
+        elev =  hardwareMap.dcMotor.get("elev");//
+        elev2 = hardwareMap.dcMotor.get("elev2");//expansion 1
         //MOTORES EXPANSION
 
-        Carpus = hardwareMap.servo.get("Carpus");
-        DeChill = hardwareMap.servo.get ("DeChill");
-        DeChill2 = hardwareMap.servo.get("DeChill2");
-        Garrita = hardwareMap.servo.get("Garrita");
-        Burgir  = hardwareMap.servo.get("burigir");
+        garra = hardwareMap.servo.get("garra");//control 0
+        rotador = hardwareMap.servo.get("rotador");// expansion 0
 
     }
 
     @Override
     public void loop() {
-        Elev.setPower(gamepad2.left_stick_y);
-        Elev2.setPower(gamepad2.left_stick_y);
-        Brazito.setPower(gamepad2.right_stick_y);
-        Elev3.setPower(gamepad2.right_trigger);
-        Elev3.setPower(-gamepad2.left_trigger);
 
-        if (gamepad2.a) {
-            Carpus.setPosition(1);
-        }
-
-        if (gamepad2.b)
-            Carpus.setPosition(0);
+        elev2.setPower(gamepad2.left_stick_y);
+        brazo.setPower(gamepad2.right_stick_y);
+        
 
 
         if (gamepad2.x) {
-            Burgir.setPosition(0.51);
+            garra.setPosition(1);
         } else {
-            Burgir.setPosition(1);
+            garra.setPosition(0.50);
 
             if (gamepad2.b) {
-                Garrita.setPosition(0);
+                garra.setPosition(0);
             }
 
             if (gamepad2.b) {
-                Garrita.setPosition(0);
+                garra.setPosition(0);
             }
             if (gamepad2.left_bumper) {
-                Garrita.setPosition(1);
+                garra.setPosition(1);
             }
 
             double drive = -gamepad1.left_stick_y;
@@ -103,10 +87,10 @@ public class TotiosEvo extends OpMode {
             }
 
 
-            Izqe.setPower(speeds[0]);
-            Dere.setPower(speeds[1]);
-            Izqa.setPower(-speeds[2]);
-            Dera.setPower(speeds[3]);
+            izqE.setPower(speeds[0]);
+            derE.setPower(speeds[1]);
+            izqA.setPower(-speeds[2]);
+            derA.setPower(speeds[3]);
 
         }
     }
